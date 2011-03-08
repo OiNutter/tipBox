f4823496789a3321bd229daa99a986bb814a5210
OiNutter <willmckenzie@oinutter.co.uk>
2011-03-08 14:56:59 +0000

    added mouse tracking
    added event helper methods

--- src/tipBox.js
+++ src/tipBox.js
@@ -14,6 +14,18 @@ var tipBox = (function(element,text,options){
          _showTimer,
          _hideTimer,
          _visible = false,
+         _addEvent = function(el,event,callback){
+		 	if(el.addEventListener)
+		 		el.addEventListener(event,callback,false);
+		 	else
+		 		el.attachEvent(event,callback);
+	 	 },
+	 	 _removeEvent = function(el,event,callback){
+	 		 if(el.removeEventListener)
+	 			 el.removeEventListener(event,callback,false);
+	 		 else
+	 			 el.detachEvent(event,callback);
+	 	 },
          _getObjKeys = function(obj){
 	   			if(typeof obj != 'Object')
 	   				return [];
@@ -58,73 +70,92 @@ var tipBox = (function(element,text,options){
               
          },
          _options = _mergeOptions({
-        	 				trackMouse:false,
-        	 				lockTo: 'top-right',
+        	 				snapTo:'element',
+        	 				position: 'top-right',
                             showOn: 'mouseover',
                             showDelay:1,
                             hideOn: 'mouseout',
                             hideDelay:1,
                             template: "<div class='tipBox'>[text]</div>"},options),
-         _position = function(){
-        	if(_options.trackMouse){
-        		
-        	} else {
+         _position = function(e){
+        	 var snapTo = {},
+        	 	docElement = document.documentElement,
+        	 	body = document.body || { scrollLeft: 0 };
+        	 	
+        	if(_options.snapTo=='mouse')
+        		snapTo = {
+    					'top':e.pageY || (e.clientY + (docElement.scrollTop || body.scrollTop) - (docElement.clientTop || 0)),
+    					'left': e.pageX || (e.clientX + (docElement.scrollLeft || body.scrollLeft) - (docElement.clientLeft || 0)),
+    					'width':0,
+    					'height':0
+    				};
+           	else
+        		snapTo = {
+    					'top':_target.offsetTop,
+    					'left': _target.offsetLeft,
+    					'width':_target.offsetWidth,
+    					'height':_target.offsetHeight
+    				};
+        	
         		//get y position
-        		if(_options.lockTo.indexOf('top')!=-1)
-        			_tipBox.style.top = _target.offsetTop - _tipBox.offsetHeight + "px";
-        		else if(_options.lockTo.indexOf('bottom')!=-1)
-        			_tipBox.style.top = _target.offsetTop + _target.offsetHeight + "px";
+        		if(_options.position.indexOf('top')!=-1)
+        			_tipBox.style.top = snapTo.top - _tipBox.offsetHeight + "px";
+        		else if(_options.position.indexOf('bottom')!=-1)
+        			_tipBox.style.top = snapTo.top + snapTo.height + "px";
         		else
-        			_tipBox.style.top = _target.offsetTop - ((_tipBox.offsetHeight-_target.offsetHeight)/2) + "px";
+        			_tipBox.style.top = snapTo.top - ((_tipBox.offsetHeight-snapTo.height)/2) + "px";
         		
         		//get x position
-        		if(_options.lockTo.indexOf('left')!=-1)
-        			_tipBox.style.left = _target.offsetLeft - _tipBox.offsetWidth + "px";
-        		else if(_options.lockTo.indexOf('right')!=-1)
-        			_tipBox.style.left = _target.offsetLeft + _target.offsetWidth + "px";
+        		if(_options.position.indexOf('left')!=-1)
+        			_tipBox.style.left = snapTo.left - _tipBox.offsetWidth + "px";
+        		else if(_options.position.indexOf('right')!=-1)
+        			_tipBox.style.left = snapTo.left + snapTo.width + "px";
         		else
-        			_tipBox.style.left = _target.offsetLeft - ((_tipBox.offsetWidth-_target.offsetWidth)/2) + "px";
+        			_tipBox.style.left = snapTo.left - ((_tipBox.offsetWidth-snapTo.width)/2) + "px";
+        		
+        	if(_options.snapTo=='mouse'){
+        		_addEvent(_target,'mousemove',_position);
+        		_addEvent(_tipBox,'mousemove',_position);
         	}
          },
-         _draw = function(){
+         _draw = function(e){
         	_tipBox = document.createElement('div');
           	_tipBox.innerHTML = _parseTxt();
           	_tipBox.style.position = 'absolute';
             	_target.parentNode.insertBefore(_tipBox,_target.nextSibling);
-            	_position();
-            if(_tipBox.addEventListener){
-            	_tipBox.addEventListener('mouseover',function(e){window.clearTimeout(_hideTimer);},false);
-            	_tipBox.addEventListener('mouseout',hide,false);
-            } else {
-            	_tipBox.attachEvent('mouseover',function(e){window.clearTimeout(_hideTimer);},false);
-            	_tipBox.attachEvent('mouseout',hide);
+            	_position(e);
+            if(_options.hideOn=='mouseout'){
+            	_addEvent(_tipBox,'mouseover',function(e){window.clearTimeout(_hideTimer)});
+               	_addEvent(_tipBox,'mouseout',hide);
             }
             _visible = true;
          },
          _remove = function(){
         	 _target.parentNode.removeChild(_target.nextSibling);
+        	 if(_options.snapTo=='mouse')
+        		 _removeEvent(_target,'mousemove',_position);
         	 _tipBox = null;
         	 _visible = false;
          },
-         show = function(){
+         show = function(e){
         	if(!_visible)
-               	_showTimer = setTimeout(_draw,_options.showDelay*1000);
+               	_showTimer = setTimeout(_draw,_options.showDelay*1000,e);
         	else
         		window.clearTimeout(_hideTimer);
          },
-         hide = function(){
+         hide = function(e){
         	 if(_visible)
         		 _hideTimer = setTimeout(_remove,_options.hideDelay*1000);
         	 else
         		 window.clearTimeout(_showTimer);
          };
-      if(_target.addEventListener){          
-        _target.addEventListener(_options.showOn, show,false);
- 		_target.addEventListener(_options.hideOn, hide,false);
-      } else {
-    	_target.attachEvent(_options.showOn, show);
-   		_target.attachEvent(_options.hideOn, hide);  
-      }
+
+         
+         if(_options.snapTo=='mouse')
+        	 _options.hideDelay=0;
+         
+         _addEvent(_target,_options.showOn,show);
+         _addEvent(_target,_options.hideOn,hide);
 
 return {
     show:show,
